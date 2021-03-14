b4a0d3e65abc196ae800214f754f9499e23c5de4
ashi0419 <andyshi@buffalo.edu>
2021-03-14 17:25:54 -0400

    Added AWS S3 info for image hosting

--- class_project/settings.py
+++ class_project/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
+    'django.contrib.staticfiles',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,13 @@ LOGIN_URL = 'login'
 # For Uploading Profile Picture
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+#AWS S3 information (Image hosting) 
+AWS_ACCESS_KEY_ID = 'AKIATTWR7B7R2P6PFTNN'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'cse312-project-files'
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
